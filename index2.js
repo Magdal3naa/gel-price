6b2a3b90425431ecf2106b2877ededf699521645
Magdalena Fichtl <magdalenafichtl@Magdalenas-MacBook-Pro.local>
2023-06-04 21:45:16 +0200

    text updated

--- index2.js
+++ index2.js
@@ -3,7 +3,7 @@ async function getPrice(url) {
       const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=gelato&vs_currencies=usd');
       const data = await response.json();
       const price = data.gelato.usd;
-      document.getElementById('result').textContent = `The price of Gelato is: $${price}`;
+      document.getElementById('result').textContent = `The price of $GEL is: $${price}`;
     } catch (error) {
       console.log('An error occurred:', error);
     }
